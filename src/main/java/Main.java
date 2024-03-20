d7826f018e95e726f842f74ea592fc2adbedd300
Gigakoks1337 <162459201+Gigakoks1337@users.noreply.github.com>
2024-03-20 13:41:38 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "Hello World";
+        for (int i = 0; i < text.length(); i++) {
+            System.out.println(text.charAt(i));
+        }
+    }
+}
